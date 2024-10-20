5c47360975bd99beea07837e6872644926eaa653
Rainer Simon <rainer@rainersimon.io>
2024-10-20 12:20:36 +0200

    Build tweaks

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import sveltePreprocess from 'svelte-preprocess';
+import { sveltePreprocess } from 'svelte-preprocess';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
